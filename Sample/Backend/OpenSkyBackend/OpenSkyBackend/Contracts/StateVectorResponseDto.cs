344e80c95be5f9a654078dff2e4018821ba6a233
Philipp Wagner <pwag86@googlemail.com>
2020-09-25 21:49:28 +0200

    Initial Backend
    Prepare a Backend to allow Polling for results using an SPA.

---
+++ Sample/Backend/OpenSkyBackend/OpenSkyBackend/Contracts/StateVectorResponseDto.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Philipp Wagner. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Text.Json.Serialization;
+
+namespace OpenSkyBackend.Contracts
+{
+    public class StateVectorResponseDto
+    {
+        [JsonPropertyName("time")]
+        public int Time { get; set; }
+
+        [JsonPropertyName("states")]
+        public StateVectorDto[] States { get; set; }
+    }
+}
---
+++ Sample/Backend/OpenSkyBackend/OpenSkyBackend/Startup.cs
@@ -0,0 +1,57 @@
+// Copyright (c) Philipp Wagner. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using OpenSkyBackend.Options;
+
+namespace OpenSkyBackend
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOptions();
+
+            ConfigureOptions(services);
+
+            services.AddControllers();
+        }
+
+        private void ConfigureOptions(IServiceCollection services)
+        {
+            services.Configure<ApplicationOptions>(Configuration.GetSection("Application"));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
